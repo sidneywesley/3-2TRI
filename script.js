8cf852ca354923f3d255417830b378ae818caaba
sidneywesley <130581784+sidneywesley@users.noreply.github.com>
2024-07-01 09:52:47 -0300

    77

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
 {
+
     enunciado: "Pergunta 1",
     alternativas: [
         "Alternativa 1",
